dd4c068ca6f28194f2283d8d3f953c99dd55a560
Parth <67057645+parth-io@users.noreply.github.com>
2021-02-13 23:35:50 +0800

    Update

--- .bin/PushToRemote.sh
+++ .bin/PushToRemote.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git add /media/parth-dev/b14cdd0c-608f-4c5c-9b00-2d2a5c6978b0/parth/Desktop/Coding/web-crawler-nodejs/
+git add /home/parth/Desktop/Coding/web-crawler-nodejs/
 git commit -m "Update"
 git push origin main
 exit 0
